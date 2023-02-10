f680ae9f7a7aa8d4333867426c0b46a644ce75ef
Ole Schönburg <ole.schoenburg@gmail.com>
2023-02-10 15:15:05 +0100

    First prototype

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
-## Developing
+## Initial Setup
 
-Run `just hack` to start kind, deploy chaos-mesh and camunda-platform.
-Run `k -n chaos-mesh port-forward services/chaos-dashboard 2333` to gain acess to the chaos-mesh dashboard.
+The cluster should have chaos mesh installed:
+```shell
+helm list -A -f chaos
+```
+
+If not, you can install it:
+
+```shell
+helm upgrade --install chaos-mesh \
+    chaos-mesh/chaos-mesh --version 2.5.1 \
+    --namespace=chaos-mesh --create-namespace \
+    --set dashboard.securityMode=false \
+    --set chaosDaemon.runtime=containerd \
+    --set chaosDaemon.socketPath=/var/run/containerd/containerd.sock
+```
+
+Accessing chaos mesh:
+
+```
+kubectl -n chaos-mesh port-forward svc/chaos-dashboard 2333
+```
+
+## Running experiments
+
+### Locally using nix
+
+Enter the nix develop environment:
+```shell
+nix develop
+```
+
+Run the script:
+```shell
+BENCHMARK_NAME=os-perf-test ./run.sh
+```
+
+This will take a moment but eventually the script will print out some measurements before and after running the chaos-mesh experiment.
+
+### Locally without nix
+
+Make sure that you have all dependencies installed:
+- gcloud
+- kubectl
+- helm
+- jq
+
+and run the script: 
+```shell
+BENCHMARK_NAME=os-perf-test ./run.sh
+```
+
+### Using GitHub Actions
+
+Trigger the `measure` workflow with a benchmark name.
+
+## Adding experiments
+
+Access the chaos mesh UI, use the experiment designer and save the yaml file in `experiments/`. 
+Replace namespaces and the like with enviroment variables. These will be substituted before the experiment is deployed.
---
+++ run.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+set -o errexit
+
+setup() {
+    helm repo add zeebe-benchmark https://zeebe-io.github.io/benchmark-helm
+    helm repo add chaos-mesh https://charts.chaos-mesh.org
+    helm upgrade --install chaos-mesh \
+        chaos-mesh/chaos-mesh --version 2.5.1 \
+        --namespace=chaos-mesh --create-namespace \
+        --set dashboard.securityMode=false \
+        --set chaosDaemon.runtime=containerd \
+        --set chaosDaemon.socketPath=/var/run/containerd/containerd.sock
+    helm upgrade --install "$BENCHMARK_NAME" \
+        zeebe-benchmark/zeebe-benchmark \
+        --namespace="$BENCHMARK_NAME" --create-namespace
+
+    kubectl ns "$BENCHMARK_NAME"
+    kubectl create job --from=cronjob/leader-balancer manual-rebalancing 
+}
+
+measure() {
+    url=https://monitoring.googleapis.com/v1/projects/zeebe-io/location/global/prometheus/api/v1/query
+    token=$(gcloud auth print-access-token)
+
+    # Queries
+    process_latency_99="histogram_quantile(0.99, sum(rate(zeebe_process_instance_execution_time_bucket{namespace=\"$BENCHMARK_NAME\"}[5m])) by (le))"
+    process_latency_90="histogram_quantile(0.90, sum(rate(zeebe_process_instance_execution_time_bucket{namespace=\"$BENCHMARK_NAME\"}[5m])) by (le))"
+    process_latency_50="histogram_quantile(0.50, sum(rate(zeebe_process_instance_execution_time_bucket{namespace=\"$BENCHMARK_NAME\"}[5m])) by (le))"
+
+    p99=$(curl -sSL $url -d "query=$process_latency_99" -H "Authorization: Bearer $token" | jq '.data.result[0].value[1] | tonumber')
+    p90=$(curl -sSL $url -d "query=$process_latency_90" -H "Authorization: Bearer $token" | jq '.data.result[0].value[1] | tonumber')
+    p50=$(curl -sSL $url -d "query=$process_latency_50" -H "Authorization: Bearer $token" | jq '.data.result[0].value[1] | tonumber')
+    printf "Process Instance Execution Time: p99=%s p90=%s p50=%s\n", "$p99", "$p90", "$p50" 
+}
+
+setup
+sleep 5m
+measure
+envsubst < experiments/network-latency-35.yaml | kubectl apply -f -
+sleep 5m
+measure
